09c7093f9aad335d2a8d84d09e4da673d9e0d8c2
ImGwangHwan <ghlim100@naver.com>
2026-05-07 11:51:53 +0900

    [Passed] 좌표평면 위의 균형 2 (77ms, 17MB)

--- trail2/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
+++ trail2/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.py
@@ -1,39 +1,36 @@
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-x_points = [p[0] for p in points]
-y_points = [p[1] for p in points]
+# x, y 좌표의 최댓값을 구함 (루프 범위 설정용)
+max_x = max(p[0] for p in points)
+max_y = max(p[1] for p in points)
 
-answer = []
-min_val = float('inf')
+min_m = float('inf') # 최댓값 M의 최솟값을 저장할 변수
 
-for i in range(0, max(x_points) + 1, 2):
-    for j in range(0, max(y_points) + 1, 2):
-        x_line = i
-        y_line = j
-
-        # 좌하단, 우하단, 좌상단, 우상단
+# x축과 y축에 평행한 직선은 항상 짝수에 그어짐
+for x_line in range(0, max_x + 2, 2):
+    for y_line in range(0, max_y + 2, 2):
+        
+        # 4개 구역의 점 개수 초기화 (좌하, 우하, 좌상, 우상)
         div = [0, 0, 0, 0]
         
         for x, y in points:
-            if 0 <= x < x_line and 0 <= y < y_line:
+            if x < x_line and y < y_line:
                 div[0] += 1
-            if x > x_line and 0 <= y < y_line:
+            elif x > x_line and y < y_line:
                 div[1] += 1
-            if 0 <= x < x_line and y > y_line:
+            elif x < x_line and y > y_line:
                 div[2] += 1
-            if x > x_line and y > y_line:
+            elif x > x_line and y > y_line:
                 div[3] += 1
+        
+        # 현재 직선(x_line, y_line)에서의 가장 많은 점의 개수 M
+        current_max = max(div)
+        
+        # 전체 경우 중 M이 가장 작을 때를 찾음
+        if current_max < min_m:
+            min_m = current_max
 
-        val = 0
-        for k in range(4):
-            val += max(div) - div[k]
-            min_val = min(min_val, val)
-            answer.append((min_val, max(div)))
-
-answer.sort()
-print(answer[0][1])
-
+print(min_m)
 
     
