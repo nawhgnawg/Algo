bdd52a180f08a01261fcfc2a45e43a003bc4bb78
ImGwangHwan <ghlim100@naver.com>
2026-05-09 22:09:33 +0900

    [Passed] 가장 가까운 두 점 사이의 거리 (48ms, 16MB)

--- trail2/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
+++ trail2/가장 가까운 두 점 사이의 거리/closest-pair-of-points.py
@@ -2,15 +2,20 @@ from itertools import combinations
 
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
-x = [p[0] for p in points]
-y = [p[1] for p in points]
 
 min_dist = float('inf')
 for point in combinations(points, 2):
     (x1, y1), (x2, y2) = point
-    dist = abs(x1 - x2) ** 2 + abs(y1 - y2) ** 2
+    dist = (x1 - x2) ** 2 + (y1 - y2) ** 2
     min_dist = min(min_dist, dist)
 
 print(min_dist)
 
 
+# min_dist = float('inf')
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         x1, y1 = points[i]
+#         x2, y2 = points[j]
+#         dist = (x1 - x2) ** 2 + (y1 - y2) ** 2
+#         min_dist = min(min_dist, dist)
