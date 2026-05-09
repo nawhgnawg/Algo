101e8071f23866a320fafb4458d71da885d845c6
ImGwangHwan <ghlim100@naver.com>
2026-05-09 19:23:53 +0900

    [Passed] 좌표평면 위의 특정 구역 2 (48ms, 16MB)

--- trail2/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ trail2/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,30 +1,34 @@
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
-x = [p[0] for p in points]
-y = [p[1] for p in points]
 
+min_area = float('inf')
 
-# 남은 점들로 만들 수 있는 사각형 = (최대 x - 최소 x) * (최대 y - 최소 y)
-rectangles = []
+for i in range(n):
+    # 인덱스 하나만 제거
+    remaining = [points[j] for j in range(n) if j != i]
 
-for x, y in points:
-    dis_x = x
-    dis_y = y
+    xs = [x for x, y in remaining]
+    ys = [y for x, y in remaining]
 
-    curr_x = []
-    curr_y = []
-    for r, c in points:
-        if r == x and c == y:
-            continue
-        curr_x.append(r)
-        curr_y.append(c)
+    area = (max(xs) - min(xs)) * (max(ys) - min(ys))
+    min_area = min(min_area, area)
+
+print(min_area)
+
+# rectangles = []
+# for x, y in points:
+#     dis_x = x
+#     dis_y = y
+
+#     curr_x = []
+#     curr_y = []
+#     for r, c in points:
+#         if r == x and c == y:
+#             continue
+#         curr_x.append(r)
+#         curr_y.append(c)
     
-    width = (max(curr_x) - min(curr_x)) * (max(curr_y) - min(curr_y))
-    rectangles.append(width)
+#     width = (max(curr_x) - min(curr_x)) * (max(curr_y) - min(curr_y))
+#     rectangles.append(width)
 
-print(min(rectangles))
-
-    
-    
-
-
+# print(min(rectangles))
