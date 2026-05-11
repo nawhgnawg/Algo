675bd8b6d646ff3f516c0a9861a67e01e41fae60
ImGwangHwan <ghlim100@naver.com>
2026-05-11 20:18:02 +0900

    [Passed] 두 방향 탈출 가능 여부 판별하기 (64ms, 17MB)

--- trail4/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ trail4/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -3,28 +3,52 @@ from collections import deque
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
+visited = [[False] * m for _ in range(n)]
+
 dx = [1, 0]
 dy = [0, 1]
 
-
-q = deque([(0, 0)])
-visited = [[False] * m for _ in range(n)]
-visited[0][0] = True
-
-ans = 0
-while q:
-    x, y = q.popleft()
-
+# DFS
+def dfs(x, y):
     if x == n - 1 and y == m - 1:
-        ans = 1
-        break
+        return True
 
     for i in range(2):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and grid[nx][ny] != 0:
-            visited[nx][ny] = True
-            q.append((nx, ny))
+        if 0 <= nx < n and 0 <= ny < m:
+            if grid[nx][ny] == 1 and not visited[nx][ny]:
+                visited[nx][ny] = True
+                if dfs(nx, ny):
+                    return True
 
-print(ans)
+    return False
+
+if dfs(0, 0):
+    print(1)
+else:
+    print(0)
+
+# BFS
+# q = deque([(0, 0)])
+# visited = [[False] * m for _ in range(n)]
+# visited[0][0] = True
+
+# ans = 0
+# while q:
+#     x, y = q.popleft()
+
+#     if x == n - 1 and y == m - 1:
+#         ans = 1
+#         break
+
+#     for i in range(2):
+#         nx = x + dx[i]
+#         ny = y + dy[i]
+
+#         if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny] and grid[nx][ny] != 0:
+#             visited[nx][ny] = True
+#             q.append((nx, ny))
+
+# print(ans)
