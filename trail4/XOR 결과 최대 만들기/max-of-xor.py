8f9091867ceac650b33256cc015bbaf2b6427bbc
ImGwangHwan <ghlim100@naver.com>
2026-05-28 17:40:49 +0900

    [Passed] XOR 결과 최대 만들기 (130ms, 21MB)

--- trail4/XOR 결과 최대 만들기/max-of-xor.py
+++ trail4/XOR 결과 최대 만들기/max-of-xor.py
@@ -1,45 +1,45 @@
 # 순열
-from itertools import combinations
+# from itertools import combinations
 
-n, m = map(int, input().split())
-A = list(map(int, input().split()))
-
-max_xor = 0
-
-for com in combinations(A, m):
-    curr_xor = 0
-    for num in com:
-        curr_xor ^= num
-    max_xor = max(curr_xor, max_xor)
-
-print(max_xor)
-
-
-# 백트래킹
 # n, m = map(int, input().split())
 # A = list(map(int, input().split()))
 
-# answer = []
-
 # max_xor = 0
 
-# def bt(start):
-#     global max_xor
-
-#     if len(answer) == m:
-#         # XOR 계산
-#         curr_xor = 0
-#         for num in answer:
-#             curr_xor = curr_xor ^ num
-#         max_xor = max(curr_xor, max_xor)
-#         return
-
-#     for i in range(start, n):
-#         answer.append(A[i])
-#         bt(i + 1)
-#         answer.pop()
-
-# bt(0)
+# for com in combinations(A, m):
+#     curr_xor = 0
+#     for num in com:
+#         curr_xor ^= num
+#     max_xor = max(curr_xor, max_xor)
 
 # print(max_xor)
+
+
+# 백트래킹
+n, m = map(int, input().split())
+A = list(map(int, input().split()))
+
+answer = []
+
+max_xor = 0
+
+def bt(start):
+    global max_xor
+
+    if len(answer) == m:
+        # XOR 계산
+        curr_xor = 0
+        for num in answer:
+            curr_xor = curr_xor ^ num
+        max_xor = max(curr_xor, max_xor)
+        return
+
+    for i in range(start, n):
+        answer.append(A[i])
+        bt(i + 1)
+        answer.pop()
+
+bt(0)
+
+print(max_xor)
 
