6b67cb8e0583874cd06c8f100ed39bf3ff757597
ImGwangHwan <ghlim100@naver.com>
2026-06-29 12:10:02 +0900

    [Passed] 계속 중첩되는 사각형 (59ms, 17MB)

--- trail2/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ trail2/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -7,25 +7,24 @@ n = int(input())
 
 is_blue = False
 
-for _ in range(n):
+for i in range(n):
     x1, y1, x2, y2 = map(int, input().split())
     x1, y1, x2, y2 = x1 + OFFSET, y1 + OFFSET, x2 + OFFSET, y2 + OFFSET
 
-    for i in range(x1, x2):
-        for j in range(y1, y2):
-            if is_blue:
-                grid[i][j] = 1
-            else:
-                grid[i][j] = 0
-    if not is_blue:
-        is_blue = True
+    if i % 2 == 0:
+        color = 1  # 빨간색
     else:
-        is_blue = False
+        color = 2  # 파란색
+
+    # 격자 칸 정석대로 색칠하기
+    for r in range(x1, x2):
+        for c in range(y1, y2):
+            grid[r][c] = color
 
 total_area = 0
 for i in range(MAX_R + 1):
     for j in range(MAX_R + 1):
-        if grid[i][j] == 1:
+        if grid[i][j] == 2:
             total_area += 1
 
 print(total_area)
