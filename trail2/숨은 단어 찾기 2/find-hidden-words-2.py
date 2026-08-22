3723d6f39ecd2ee6077a0a2cdd2ac613ca720d09
ImGwangHwan <ghlim100@naver.com>
2026-08-22 11:50:05 +0900

    [Passed] 숨은 단어 찾기 2 (73ms, 18MB)

--- trail2/숨은 단어 찾기 2/find-hidden-words-2.py
+++ trail2/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -9,19 +9,23 @@ answer = 0
 
 for i in range(n):
     for j in range(m):
+        # 'L'로 시작하지 않으면 패스!
         if grid[i][j] != 'L':
             continue
         
-        char = grid[i][j]
-        
+        # 8가지 방향을 모두 찔러봅니다.
         for k in range(8):
-            curr_str = char
+            curr_str = 'L'
             for l in range(1, 3):
                 nx = i + dx[k] * l
                 ny = j + dy[k] * l
+                # 범위 체크
                 if 0 <= nx < n and 0 <= ny < m:
                     curr_str += grid[nx][ny]
+                else:
+                    break   # 격자를 벗어나면 더 이상 글자를 안 붙이고 멈춤
 
+            # 조립된 3글자가 'LEE'라면 정답 카운트!
             if len(curr_str) == 3 and curr_str == 'LEE':
                     answer += 1
 
