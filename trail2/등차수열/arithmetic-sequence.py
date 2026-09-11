f6e957758475a04f9b9d74391e84d1a5750a4735
ImGwangHwan <ghlim100@naver.com>
2026-09-11 22:01:54 +0900

    [Passed] 등차수열 (59ms, 17MB)

--- trail2/등차수열/arithmetic-sequence.py
+++ trail2/등차수열/arithmetic-sequence.py
@@ -1,17 +1,32 @@
+from collections import Counter
+
 n = int(input())
 a = list(map(int, input().split()))
 
-max_pairs = 0
+midpoint_counts = Counter()
 
-for k in range(1, max(a) + 1):
-    pairs = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            if a[j] - k == k - a[i]:
-                pairs += 1
+# 두 수의 합이 짝수이면 그 중점 K = (a_i + a_j) // 2 가 결정된다
+# 모든 쌍 (i < j)을 확인하며 가능한 K의 빈도수를 누적
+for i in range(n):
+    for j in range(i + 1, n):
+        total = a[i] + a[j]
+        if total % 2 == 0:
+            midpoint_counts[total // 2] += 1
 
-    max_pairs = max(max_pairs, pairs)
+print(max(midpoint_counts.values()) if midpoint_counts else 0)
 
-print(max_pairs)
+
+# max_pairs = 0
+
+# for k in range(1, max(a) + 1):
+#     pairs = 0
+#     for i in range(n):
+#         for j in range(i + 1, n):
+#             if a[j] - k == k - a[i]:
+#                 pairs += 1
+
+#     max_pairs = max(max_pairs, pairs)
+
+# print(max_pairs)
 
         
